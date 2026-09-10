64108a1bc8fef30286ad745ce03af615f5ecd4f4
kawae11 <sdd9422s89@privaterelay.appleid.com>
2026-09-10 10:05:35 +0500

    w

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -10,6 +10,7 @@ namespace Lab2
         public int Task1(int n, int p, int h)
         {
             int answer = 0;
+            int dp = p;
             int sum = 0;
             for (int i = 0; i < n; i++)
             {
--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7960fa1e24f893e14bec8bcc94ea54bbdeaf5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d906ce6fa4489cb22d53cfe149b316efa89ec69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
