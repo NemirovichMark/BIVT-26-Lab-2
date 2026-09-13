d42664930728d960423806e8d37223849e8a8605
skartich <skart0938@gmail.com>
2026-09-13 23:38:05 +0300

    Purple.cs

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
@@ -12,7 +12,10 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                answer += (p + i*h) * (p + i*h);
+            }
             // end
 
             return answer;
@@ -23,7 +26,16 @@ namespace Lab2
             int remainder = 0;
 
             // code here
-
+            while (a >= b)
+            {
+                a -= b;
+                quotient += 1;
+                remainder = a;
+            }
+            if (a < b)
+            {
+                remainder = a;
+            }
             // end
 
             return (quotient, remainder);
@@ -33,8 +45,25 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double a1 = 1; double b1 = 1;
+            double a2 = 2; double b2 = 1;
+            double x1 = a1 / b1;
+            double x2 = a2 / b2;
+            var tenM = 0.0001;
+            while (Math.Abs(x1-x2) > tenM)
+            {
+                double af = a1 + a2;
+                double bf = b1 + b2;
+                a1 = a2;
+                b1 = b2;
+                a2 = af;
+                b2 = bf;
+                x1 = x2;
+                x2 = af/bf;
+            }
+            answer = x2;
             // end
+
 
             return answer;
         }
@@ -43,7 +72,12 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            var tenM = 0.0001;
+            for (int n = 0; Math.Abs(b*Math.Pow(q,n)) > tenM; n++)
+            {
+                answer++;
+            }
+            answer++; // т.к. цикл заканчивается за действие до получение нужного значения < tenM
             // end
 
             return answer;
@@ -53,7 +87,17 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            long number = a;
+            while (b > 0)
+            {
+                number *= b;
+                b--;
+            }
+            while (number >= 10)
+            {
+                number /= 10;
+                answer++;
+            }
             // end
 
             return answer;
@@ -63,7 +107,13 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            double semki = 0;
+            for (double i = 0; i < 64; i++)
+            {
+                semki += Math.Pow(2,i);
+            }
+            semki /= 15000000;
+            answer = (long)semki;
             // end
 
             return answer;
@@ -74,7 +124,18 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            double _summMec = S*(100/d)/12;
+            double _2xSum = S*2;
+            for (int i = 0; S < _2xSum; i++)
+            {
+                S += _summMec;
+                if (i % 12 == 0 && i != 0)
+                {
+                    _summMec = S*(100/d)/12;
+                }
+                answer = i;
+            }
+            
             // end
 
             return answer;
