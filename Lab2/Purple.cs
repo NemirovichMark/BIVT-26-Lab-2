54aef451029f404e766a1a82c7ecab0241e3395c
Yarkat <reklamy00@list.ru>
2026-09-24 12:47:40 +0300

    Implement tasks in Purple class methods

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -12,7 +12,16 @@ namespace Lab2
             int answer = 0;
 
             // code here
+            // code here
+int s = 0;
 
+for (int i = 0; i < n; i++)
+{
+    s += (p + i * h) * (p + i * h);
+}
+
+return s;
+// end
             // end
 
             return answer;
@@ -23,7 +32,12 @@ namespace Lab2
             int remainder = 0;
 
             // code here
-
+    while(a >= b)
+    {
+        a = a - b;
+        quotient++;
+    }
+    remainder = a;
             // end
 
             return (quotient, remainder);
@@ -33,37 +47,80 @@ namespace Lab2
             double answer = 0;
 
             // code here
+int a = 1, b = 2;
+int c = 1, d = 1;
 
+double p = (double)a / c;
+double q = (double)b / d;
+
+while (Math.Abs(q - p) >= 0.0001)
+{
+    a = a + b;
+    c = c + d;
+
+    p = q;
+    q = (double)a / c;
+
+    int t = b;
+    b = a;
+    a = t;
+
+    t = d;
+    d = c;
+    c = t;
+}
+
+return q;
             // end
 
             return answer;
         }
         public int Task4(double b, double q)
         {
-            int answer = 0;
+             int answer = 1;
+ double x = b;
 
-            // code here
+ while (Math.Abs(x) >= 0.0001)
+ {
+     x = x * q;
+     answer++;
+ }   
 
-            // end
-
-            return answer;
+ return answer;    
         }
         public int Task5(int a, int b)
         {
             int answer = 0;
 
             // code here
-
+while(b>0)
+{
+    number *= b;
+    b--;
+}
+while (number >= 10)
+{
+    number /= 10;
+    answer++;
+}
             // end
 
             return answer;
         }
         public long Task6()
         {
+            
             long answer = 0;
-
+            decimal s = 0;
+            decimal z = 1;
             // code here
-
+            for(int i = 1;i <=64 ;i++)
+            {
+                
+                s += z;
+                z *= 2;
+            }
+            return (long)(s / 15 / 1000000);
             // end
 
             return answer;
@@ -71,24 +128,53 @@ namespace Lab2
 
         public int Task7(double S, double d)
         {
-            int answer = 0;
+           int answer = 0;
+double s = S;
+double p = S * d / 100;
 
-            // code here
+// code here
+while (s < 2 * S)
+{
+    s += p / 12;
+    answer++;
 
-            // end
+    if (answer % 12 == 0)
+        p = s * d / 100;
+}
 
-            return answer;
+// end
+
+return answer;
         }
         public (double SS, double SY) Task8(double a, double b, double h)
         {
             double SS = 0;
-            double SY = 0;
+double SY = 0;
 
-            // code here
 
-            // end
+for (double x = a; x <= b; x += h)
+{
+    double s = 0;
+    double t = 1;
+    int i = 0;
 
-            return (SS, SY);
+    while (true)
+    {
+        s += t;
+
+        if (Math.Abs(t) < E)
+            break;
+
+        i++;
+
+        t = -t * x * x / ((2 * i - 1) * (2 * i));
+    }
+
+    SS += s;
+    SY += Math.Cos(x);
+}
+
+return (SS, SY);
         }
     }
 }
